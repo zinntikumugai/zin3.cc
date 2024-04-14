beff61391086c3096b6779f43b91e1cba9a8cee5
zinntikumugai <uesitananame55@gmail.com>
2024-04-14 12:22:13 +0900

    bootstrapからtailwindに移植

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,45 @@
 <div>
 
-    <div class="container text-center">
-        <div class="container pb-2" style="height: 200px;">
+    <div class="text-center">
+        <div class="pb-2" style="height: 200px;">
             <div class="pt-5"></div>
             <p>じんさん</p>
             <h1>zin3</h1>
         </div>
-        <div class="container pb-2">
+        <div class="pb-2">
             <h2>SNS</h2>
-            <div class="row">
-                <div class="col-sm-6 col-md-4 pt-5 twitter">
+            <div class="grid items-center grid-cols-1 md:grid-cols-3">
+                <div class="pt-5 pb-3 twitter">
                     <a href="https://twitter.com/uesitananame55">
                         <h3>X</h3>
                         <p>@uesitananame55</p>
                     </a>
                 </div>
-                <div class="col-sm-6 col-md-4 pt-5 bluesky">
+                <div class="pt-5 pb-3 bluesky">
                     <a href="https://bsky.app/profile/zin3.cc">
                         <h3>Bluesky</h3>
                         <p>@zin3.cc</p>
                     </a>
                 </div>
-                <div class="col-sm-6 col-md-4 pt-5 misskey">
+                <div class="pt-5 pb-3 misskey">
                     <a href="https://misskey.io/@z3">
                         <h3>Misskey.io</h3>
                         <p>@z3</p>
                     </a>
                 </div>
-                <div class="col-sm-6 col-md-4 pt-5 discord">
+                <div class="pt-5 pb-3 discord">
                     <a href="https://discord.com/users/246834514146361345">
                         <h3>Discord</h3>
                         <p>@zinntikumugai</p>
                     </a>
                 </div>
-                <div class="col-sm-6 col-md-4 pt-5 steam">
+                <div class="pt-5 pb-3 steam">
                     <a href="https://steamcommunity.com/id/zinntikumugai/">
                         <h3>Steam</h3>
                         <p>zinntikumugai</p>
                     </a>
                 </div>
-                <div class="col-sm-6 col-md-4 pt-5 vrchat">
+                <div class="pt-5 pb-3 vrchat">
                     <a href="https://vrchat.com/home/user/usr_5a64db9b-e233-4280-804e-0e9c9df6f04e">
                         <h3>VRChat</h3>
                         <p>zin3</p>
@@ -48,16 +48,16 @@
             </div>
         </div>
 
-        <div class="container pb-2 pt-3">
+        <div class="pb-2 pt-3">
             <h2 class="">Blog</h2>
-            <div class="row">
-                <div class="col-sm-6 col-md-6 pt-5 pb-3 blog-zin3">
+            <div class="grid grid-cols-1 md:grid-cols-2">
+                <div class="pt-5 pb-3 blog-zin3">
                     <a href="https://www.zinntikumugai.com/">
                         <h3>Blog</h3>
                         <p>じんさんです!</p>
                     </a>
                 </div>
-                <div class="col-sm-6 col-md-6 pt-5 pb-3 blog-sub">
+                <div class="pt-5 pb-3 blog-sub">
                     <a href="https://sublog.zinntikumugai.com/">
                         <h3>sublog</h3>
                         <p>さぶろぐ!</p>
@@ -66,16 +66,16 @@
             </div>
         </div>
 
-        <div class="container pb-2 pt-3">
+        <div class="pb-2 pt-3">
             <h2 class="">git</h2>
-            <div class="row">
-                <div class="col-sm-6 col-md-6 pt-5 pb-3 github">
+            <div class="grid grid-cols-1 md:grid-cols-2">
+                <div class="pt-5 pb-3 github">
                     <a href="https://github.com/zinntikumugai/">
                         <h3>GitHub</h3>
                         <p>zinntikumugai</p>
                     </a>
                 </div>
-                <div class="col-sm-6 col-md-6 pt-5 pb-3 gitlab">
+                <div class="pt-5 pb-3 gitlab">
                     <a href="https://gitlab.com/zinntikumugai">
                         <h3>GitLab</h3>
                         <p>zinntikumugai</p>
@@ -84,10 +84,10 @@
             </div>
         </div>
 
-        <div class="container pb-2 pt-3">
+        <div class="pb-2 pt-3">
             <h2>gpg</h2>
-            <div class="row">
-                <div class="col-12 pt-5 pb-3 keyoxide">
+            <div class="grid grid-cols-1">
+                <div class="pt-5 pb-3 keyoxide">
                     <a href="https://keyoxide.org/hkp/961F516ECD5646B15936218CEC5C53A3AB6800E5">
                         <h3>keyoxide</h3>
                         <p>961F516ECD5646B15936218CEC5C53A3AB6800E5</p>
--- src/routes/test/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1>hogehoge</h1>
